352a276485576cb95092ef4048410cba9044c3e6
Bikash-Shrestha4958 <chester4958@gmail.com>
2022-09-29 15:29:00 +0545

    .

--- index.php
+++ index.php
@@ -6,13 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Bootstrap demo</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.2.1/dist/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
-    <style>
-        #heading {
-            background-color: greenyellow;
-            padding: 2px 0px 2px 0px;
-            text-align: center;
-        }
-    </style>
+    <link rel="stylesheet" href="custom.css">
+
 </head>
 
 <body>
@@ -136,7 +131,7 @@
             </div>
             <div class="card">
                 <div class="card-body">
-                    <table class="table">
+                    <table class="table" id="table-view">
                         <thead class="thead-dark">
                             <tr>
                                 <th scope="col">id</th>
